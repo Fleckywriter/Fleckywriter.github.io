c28faffa194d794be87fd65cf7c2d974a15184b2
Fleckywriter <81414459+Fleckywriter@users.noreply.github.com>
2021-03-26 13:18:41 +0300

    Add files via upload

---
+++ главная страница сайта.js
@@ -0,0 +1,12 @@
+function show (koren)
+{
+var elem =document.getElementById(koren);
+if (elem)
+elem.style.display = "block";
+}
+function hide (koren)
+{
+var elem =document.getElementById(koren);
+if (elem)
+elem.style.display = "none";
+}
